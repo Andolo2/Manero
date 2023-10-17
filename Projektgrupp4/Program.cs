ffdcdf1075a595d5dd1ef0c4f874cf3b1ac29cac
Daniel Grahn <daniel_grahn@live.se>
2023-10-17 15:18:17 +0200

    Updated Program file

--- Projektgrupp4/Program.cs
+++ Projektgrupp4/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Projektgrupp4.Contexts;
 
@@ -5,16 +6,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Services
 builder.Services.AddControllersWithViews();
-
-var app = builder.Build();
-
 // Context
-
-
-//builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
 
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Sql")));
 
+string connectionString = "Server=tcp:grupp4-sqlserver.database.windows.net,1433;Initial Catalog=database-1;Persist Security Info=False;User ID=SqlAdmin;Password={Bytmig123!};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+
+
+using (var connection = new SqlConnection(connectionString))
+{
+    try
+    {
+        connection.Open();
+        Console.WriteLine("Connection opened successfully.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Connection error: {ex.Message}");
+    }
+}
+
+var app = builder.Build();
 
 
 app.UseHsts();
