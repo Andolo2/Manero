4a0de465a1b42675d626be6b22d423fa788d469c
TobiasLarm97 <tobbe-l@outlook.com>
2023-10-19 15:39:25 +0200

    Added partials and ProductDetailViewModel

--- Projektgrupp4/Program.cs
+++ Projektgrupp4/Program.cs
@@ -40,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=home}/{action=index}/{id?}");
 
 app.Run();
---
+++ Projektgrupp4/ViewModels/ProductDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Projektgrupp4.ViewModels
+{
+	public class ProductDetailViewModel
+	{
+		public string ProductTitle { get; set; } = null!;
+		public decimal ProductPrice { get; set; }
+		public string ProductDescription { get; set; } = null!;
+
+		//public string ProductImageUrl?
+		//public int ProductQuantity?
+		//public ICollection ProductSize?
+		//public ICollection ProductColor
+		//public ICollection ProductRating
+
+	}
+}
