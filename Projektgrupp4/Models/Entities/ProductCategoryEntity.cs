5251af34dd2550c992b68c6b06cb57137293ddde
LinusLund <lundlinus@live.se>
2023-10-19 12:47:41 +0200

    ProductCategoryEntity,ViewModel and Page

---
+++ Projektgrupp4/Models/Entities/ProductCategoryEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projektgrupp4.Models.Entities
+{
+    public class ProductCategoryEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string CategoryName { get; set; } = null!;
+
+       // public ICollection<ProductEntity> Products { get; set; } = new HashSet<ProductEntity>(); Lägg till den här när produktTabellen är inlagd
+
+    }
+}
