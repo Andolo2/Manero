53e7b5e7db64f9b76083ab519d7a910512c69863
AndOlo2 <andreasolofsson123@gmail.com>
2023-10-24 10:43:07 +0200

    Added html/scss for wishlist partial

---
+++ Projektgrupp4/Views/Partials/WishListPartial/_wishList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projektgrupp4.Views.Partials.WishListPartial
+{
+    public class _wishListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
