46bc6f877db4908b2c53f879103c7ee33016f405
Daniel Grahn <daniel_grahn@live.se>
2023-10-27 14:06:08 +0200

    StatusCodes added

---
+++ Projektgrupp4/Enum/StatusCode.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Projektgrupp4.Enum;
+
+public enum StatusCode
+{
+    Ok = 200,
+    Created = 201,
+    NoContent = 204,
+    BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
+    NotFound = 404,
+    Conflict = 409,
+    InternalServerError = 500,
+}
