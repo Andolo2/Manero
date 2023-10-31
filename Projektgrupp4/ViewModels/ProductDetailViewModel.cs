a1d92a2f95d53bd33e44a53fcbfb29e7a3b0aa29
TobiasLarm97 <tobbe-l@outlook.com>
2023-10-31 14:17:54 +0100

    Updated ProductDetail

--- Projektgrupp4/ViewModels/ProductDetailViewModel.cs
+++ Projektgrupp4/ViewModels/ProductDetailViewModel.cs
@@ -2,15 +2,37 @@
 {
 	public class ProductDetailViewModel
 	{
-		public string ProductTitle { get; set; } = null!;
-		public decimal ProductPrice { get; set; }
-		public string ProductDescription { get; set; } = null!;
+        public int ArticleNumber { get; set; }
+        public string ProductTitle { get; set; } = null!;
+        public decimal ProductPrice { get; set; }
+        public decimal? ProductOfferPrice { get; set; }
+        public bool ProductPriceOrOffer { get; set; }
+        public string? ProductDescription { get; set; } = null!;
 
-		//public string ProductImageUrl?
-		//public int ProductQuantity?
-		//public ICollection ProductSize?
-		//public ICollection ProductColor
-		//public ICollection ProductRating
+        public ICollection<ReviewEntity> ProductReviews { get; set; } = new HashSet<ReviewEntity>();
 
-	}
+        public ICollection<ProductItemEntity> ProductEntries { get; set; } = new HashSet<ProductItemEntity>();
+
+        public ICollection<ProductCategoriesEntity> ProductCategories { get; set; } = new HashSet<ProductCategoriesEntity>();
+
+        public List<SizeEntity>? ProductSizes { get; set; }
+
+        public List<ColorEntity>? ProductColors { get; set; }
+
+        public static implicit operator ProductDetailViewModel(ProductEntity entity)
+        {
+            return new ProductDetailViewModel
+            {
+                ArticleNumber = entity.ArticleNumber,
+                ProductTitle = entity.ProductTitle,
+                ProductPrice = entity.ProductPrice,
+                ProductOfferPrice = entity.ProductOfferPrice,
+                ProductDescription = entity.ProductDescription,
+                ProductReviews = entity.ProductReviews,
+                ProductEntries = entity.ProductEntries,
+                ProductCategories = entity.ProductCategories
+            };
+        }
+
+    }
 }
