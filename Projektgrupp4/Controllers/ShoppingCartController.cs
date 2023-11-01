2f01f248782a9029ed6fdb3217bbf4b4ea3073a5
Harald Mörk Jonsson <morkjonssonharald@gmail.com>
2023-11-01 13:40:05 +0100

    Design för en varukorg som kan innehålla produkter tillagd.

--- Projektgrupp4/Controllers/ShoppingCartController.cs
+++ Projektgrupp4/Controllers/ShoppingCartController.cs
@@ -4,7 +4,14 @@ namespace Projektgrupp4.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        // Index renderar ut en tom varukorg
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        // ShoppingCart renderar ut en varukorg med produkter
+        public IActionResult ShoppingCart() 
         {
             return View();
         }
