dd51aa24d447cc5b8a22c699e4ec8beebc2350dd
TobiasLarm97 <tobbe-l@outlook.com>
2023-11-02 12:54:27 +0100

    Fixed so you can go back from productDetail

--- Projektgrupp4/ViewModels/ProductReviewsViewModel.cs
+++ Projektgrupp4/ViewModels/ProductReviewsViewModel.cs
@@ -4,6 +4,7 @@ namespace Projektgrupp4.ViewModels;
 
 public class ProductReviewsViewModel
 {
+    public int ArticleNumber { get; set; }
     public List<ProductReviewCardViewModel> Reviews { get; set; }
 
 
