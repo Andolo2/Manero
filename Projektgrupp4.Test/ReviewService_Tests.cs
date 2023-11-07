2fff8fff22060881cfa4249846cf20c77e605829
TobiasLarm97 <tobbe-l@outlook.com>
2023-11-07 15:33:51 +0100

    GetReviewAsync, green

---
+++ Projektgrupp4.Test/ReviewService_Tests.cs
@@ -0,0 +1,37 @@
+﻿
+using Moq;
+using Projektgrupp4.Models.Entities;
+using Projektgrupp4.ViewModels;
+using Projektgrupp4.Test;
+using Xunit;
+using Projektgrupp4.Interfaces;
+
+namespace Projektgrupp4.Test;
+
+public class ReviewService_Tests
+{
+    private readonly Mock<IReviewService> _reviewsServiceMock;
+
+
+    public ReviewService_Tests()
+    {
+        _reviewsServiceMock = new Mock<IReviewService>();
+
+    }
+
+    [Fact]
+    public async void GetReviewAsync_Should_Return_a_listof_ProductReviewCardViewModel()
+    {
+        //Arrange
+        var productEntity = new ProductEntity() { ArticleNumber = 1, ProductTitle = "Test", ProductPrice = 32, ProductDescription = "test" };
+
+        _reviewsServiceMock.Setup(x => x.GetReviewsAsync(productEntity.ArticleNumber)).ReturnsAsync(new List<ProductReviewCardViewModel>());
+
+
+        //act
+        var result = await _reviewsServiceMock.Object.GetReviewsAsync(productEntity.ArticleNumber);
+
+        //Assert
+        Assert.IsType<List<ProductReviewCardViewModel>>(result);
+    }
+}
