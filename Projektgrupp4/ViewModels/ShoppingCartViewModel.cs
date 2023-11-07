c7593c524253ae8bef548d3f01d7352dd13bee80
Harald Mörk Jonsson <morkjonssonharald@gmail.com>
2023-11-07 10:04:55 +0100

    ShoppingCart updated

---
+++ Projektgrupp4/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Projektgrupp4.Models.Entities;
+
+namespace Projektgrupp4.ViewModels;
+
+public class ShoppingCartViewModel
+{
+    public ICollection<ProductEntity>? Products { get; set; }
+    public bool PromocodeApplied { get; set; }
+    public int Subtotal { get; set; }
+    public int Discount { get; set; }
+    public string DeliveryPrice { get; set; } = null!;
+    public int Total { get; set; }
+}
