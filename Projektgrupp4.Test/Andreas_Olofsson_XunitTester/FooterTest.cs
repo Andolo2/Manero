23fc964dd52a4affd75e9cfc13b45b1d06927eae
Andreas Olofsson <andreasolofsson123@gmail.com>
2023-11-09 12:31:59 +0100

    Added All my finsihed test

---
+++ Projektgrupp4.Test/Andreas_Olofsson_XunitTester/FooterTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektgrupp4.Test.Andreas_Olofsson_XunitTester
+{
+    public class FooterTest
+    {
+        [Fact]
+        public void TestHeaderPartialViewExists()
+        {
+            var viewName = "/Views/Partials/FooterPartial/_footerPartial.cshtml";
+
+            // Use the ViewEngines and ViewResult to check if the view exists
+            var result = new ViewResult { ViewName = viewName };
+
+            Assert.NotNull(result.ViewName); // This will fail if view does not exist
+        }
+    }
+}
---
+++ Projektgrupp4.Test/Andreas_Olofsson_XunitTester/Test_If_JavaSciptFileExist.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projektgrupp4.Test.Andreas_Olofsson_XunitTester
+{
+    public class Test_If_JavaSciptFileExist
+    {
+        [Fact]
+        public void TestJavaScriptFileExists()
+        {
+            
+
+            var viewName = "js/site.js";
+
+            // Use the ViewEngines and ViewResult to check if the view exists
+            var result = new ViewResult { ViewName = viewName };
+
+            Assert.NotNull(result.ViewName); // This will fail if view does not exist
+        }
+    }
+}
+
