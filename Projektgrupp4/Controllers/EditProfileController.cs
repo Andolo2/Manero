a5ef600e03f99fab3b7be57d070f8494bf948bf7
William Elfström <gigref@live.com>
2023-11-16 09:50:37 +0100

    Profile and EditProfile added

---
+++ Projektgrupp4/Controllers/EditProfileController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projektgrupp4.Services;
+
+namespace Projektgrupp4.Controllers
+{
+    public class EditProfileController : Controller
+    {
+      /*  private readonly AuthenticationService _authService;
+
+        public EditProfileController(AuthenticationService authService)
+        {
+            _authService = authService;
+        }
+      */
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
