f040d21fe712018fc1ffa9832b4d65acbe8284a8
Veronica-Axelsson <veronica.axelsson@vea-art.com>
2023-11-18 17:51:58 +0100

    Tests Added

---
+++ Projektgrupp4.Test/Veronica_Axelsson_Tests/Home13_Tests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projektgrupp4.Contexts;
+using Projektgrupp4.Models.Entities;
+using Projektgrupp4.Services;
+
+namespace Projektgrupp4.Test.Veronica_Axelsson_Tests;
+
+public class Home13_Tests
+{
+    [Fact]
+    public async Task Check_If_GetAllProductsAsync_Is_working()
+    {
+        // Arrange. Given database and product service.
+        var _database = new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase(databaseName: "grupp4-sqlserver.database-1.dbo").Options;
+
+        using (var context = new DataContext(_database))
+        {
+            var productService = new ProductService(context);
+
+            // Act: When GetAllProductsAsync is called with products from database
+            ProductEntity product = new ProductEntity();
+            List<ProductEntity> products = await productService.GetAllProductsAsync();
+
+            foreach (var _product in products)
+            {
+                // Assert: Then we should get a list of products. Check for "not null", "not empty" and if "true" there is product in database
+                Assert.NotNull(_product);
+                Assert.NotEmpty(products);
+                Assert.True(products.Count > 0);
+            }
+        }
+    }
+}
---
+++ Projektgrupp4.Test/Veronica_Axelsson_Tests/Shop17_Tests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projektgrupp4.Contexts;
+using Projektgrupp4.Models.Entities;
+using Projektgrupp4.Services;
+
+namespace Projektgrupp4.Test.Veronica_Axelsson_Tests;
+
+public class Shop17_Tests
+{
+    [Fact]
+    public async Task Check_If_ProductTitle_Has_Content()
+    {
+        // Arrange. Given database and product service.
+        var _database = new DbContextOptionsBuilder<DataContext>().UseInMemoryDatabase(databaseName: "grupp4-sqlserver.database-1.dbo").Options;
+
+        using (var context = new DataContext(_database))
+        {
+            var productService = new ProductService(context);
+
+            // Act: When GetAllProductsAsync is called with products from database
+            var product = new ProductEntity();
+            List<ProductEntity> products = await productService.GetAllProductsAsync();
+
+            foreach (var _product in products)
+            {
+                // Assert: Then we should get a list of products. Check for "not null" and "not empty".       
+                Assert.NotNull(_product.ProductTitle);
+                Assert.NotEmpty(_product.ProductTitle);
+            }
+        }
+    }
+}
