761a795a733c3adc05ecf8132af61fd8a11f62ac
sanjinn <rizvan_rondic@hotmail.com>
2023-11-19 21:33:02 +0100

    Tests Done

--- Projektgrupp4.Test/Rizvan_Rondic_Tests/MyAddress_Tests.cs
+++ Projektgrupp4.Test/Rizvan_Rondic_Tests/MyAddress_Tests.cs
@@ -10,32 +10,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using Microsoft.IdentityModel.Tokens;
+using Projektgrupp4.ViewModels;
+using System.Net;
+using Projektgrupp4.Models.Entities;
 
 namespace Projektgrupp4.Test.Rizvan_Rondic_Tests;
 
 public class MyAddress_Tests
 {
     [Fact]
-    public void MyAddressControllerFileExists()
+    public void TestIfThereAreAddressesInDatabase()
     {
+
         // Arrange
-        var filePath = "I:\\WIN22\\Projekt\\Projekt\\Manero\\Projektgrupp4\\Controllers\\MyAddressController.cs"; // Hårdkodat path.
+        var options = new DbContextOptionsBuilder<DataContext>()
+            .UseInMemoryDatabase(databaseName: "TestDatabase")
+            .Options;
 
-        // Act
-        bool fileExists = File.Exists(filePath);
+        using (var context = new DataContext(options))
+        {
+            context.Adresses.Add(new AddressEntity { Title = "Address1", StreetName = "Street1", CountryName = "Sweden", CityName = "Örebro" });
+            context.Adresses.Add(new AddressEntity { Title = "Address2", StreetName = "Street2", CountryName = "Sweden", CityName = "Örebro" });
+            context.SaveChanges();
+        }
 
-        // Assert
-        Assert.True(fileExists, $"The file at '{filePath}' does not exist.");
+        using (var context = new DataContext(options))
+        {
+            var controller = new MyAddressController(context);
 
-        //// Arrange
-        //var filePath = "Controllers/MyAddressController.cs";
+            // Act
+            var result = controller.GetAddresses();
 
-        //// Act
-        //bool fileExists = File.Exists(filePath);
+            // Assert
+            Assert.IsType<ViewResult>(result);
 
-        //// Assert
-        //Assert.True(fileExists, $"The file at '{filePath}' does not exist.");
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsType<List<MyAddressViewModel>>(viewResult.Model);
+            
+            Assert.NotEmpty(model);
+
+        }
 
     }
-
 }
